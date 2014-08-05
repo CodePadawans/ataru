3f0eb486731d1846c7b60d5ae50ca85053895710
Magdalena <madziaf86@tlen.pl>
2014-08-05 15:55:59 +0200

    update define_method

--- lib/ataru/code_examples.rb
+++ lib/ataru/code_examples.rb
@@ -1,10 +1,17 @@
-class CodeExamples < MiniTest::Test
+#require 'minitest/autorun'
+
+class CodeExamples# < MiniTest::Test
   def initialize
   end
 
   def add_test_cases(code)
     code.each_with_index do |snippet, index|
-
+      self.class.send(:define_method, "test_#{index}") do 
+        puts "i'm the snippet"
+      end
     end
   end
 end
+c = CodeExamples.new
+c.add_test_cases(["puts 'blah'"])
+c.test_0
