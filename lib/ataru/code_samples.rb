2b4fbf5d404e21a7603a8a0068654a52fe2e5dae
Magdalena <madziaf86@tlen.pl>
2014-08-12 18:27:19 +0200

    clean up

--- lib/ataru/code_samples.rb
+++ lib/ataru/code_samples.rb
@@ -5,7 +5,6 @@ class CodeSamples
   def add_test_cases(code_samples)
     Class.new(MiniTest::Test) do
       code_samples.each_with_index do |sample, index|
-        puts "i'm running"
         define_method("test_#{index}") do
           eval(sample)
         end
