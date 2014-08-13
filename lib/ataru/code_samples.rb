ee91744f107b80534b75b0c8cd7b03b37cde687b
Magdalena <madziaf86@tlen.pl>
2014-08-13 15:05:35 +0200

    give names to test calsses and test methods according to md file name

--- lib/ataru/code_samples.rb
+++ lib/ataru/code_samples.rb
@@ -1,15 +1,22 @@
 require 'minitest'
+require_relative '../bin/ataru'
 
 class CodeSamples
 
   def self.add_test_cases(code_samples)
-    Class.new(MiniTest::Test) do
-      code_samples.each_with_index do |sample, index|
-        define_method("test_#{index}") do
+    klass = Class.new(MiniTest::Test) do
+      def name
+        self.class.name
+      end
+      CodeSamples.const_set(file_name, klass)
+      code_samples.each_with_index do |file_name, sample, index|
+        define_method("#{file_name}_test_#{index}") do
           eval(sample)
         end
       end
     end
+    puts klass.method(:to_s).name
+    puts klass.methods.map{ |m| m.to_s} 
   end
 end
 
