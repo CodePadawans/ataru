b9c84442aabf41951b0682a6ddba5bfa60007b3c
Magdalena <madziaf86@tlen.pl>
2014-08-15 17:48:19 +0200

    test names contain md file name

--- lib/ataru/code_samples.rb
+++ lib/ataru/code_samples.rb
@@ -1,22 +1,19 @@
 require 'minitest'
+require 'pathname'
 
 class CodeSamples
 
   def self.add_test_cases(file_name, code_samples)
+    basename = Pathname.new(file_name).basename(".*").to_s
     klass = Class.new(MiniTest::Test) do
-      def name
-        self.class.name
-      end
       code_samples.each_with_index do |sample, index|
-        define_method("#{file_name}_test_#{index}") do
+        define_method("test_#{basename}_#{index}") do
           eval(sample)
         end
       end
     end
-   #modity filename to be a class name, stick it to a var and pass this var in the next line 
-      CodeSamples.const_set(file_name, klass)
-    puts klass.name
-    puts klass.methods.map{ |m| m.to_s} 
+
+    klass
   end
 end
 
