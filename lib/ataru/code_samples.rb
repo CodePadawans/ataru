0dc42a022e65180f76511870cab0387e9655fde0
Magdalena <madziaf86@tlen.pl>
2014-08-15 15:37:08 +0200

    add test/resources files

--- lib/ataru/code_samples.rb
+++ lib/ataru/code_samples.rb
@@ -1,21 +1,21 @@
 require 'minitest'
-require_relative '../bin/ataru'
 
 class CodeSamples
 
-  def self.add_test_cases(code_samples)
+  def self.add_test_cases(file_name, code_samples)
     klass = Class.new(MiniTest::Test) do
       def name
         self.class.name
       end
-      CodeSamples.const_set(file_name, klass)
-      code_samples.each_with_index do |file_name, sample, index|
+      code_samples.each_with_index do |sample, index|
         define_method("#{file_name}_test_#{index}") do
           eval(sample)
         end
       end
     end
-    puts klass.method(:to_s).name
+   #modity filename to be a class name, stick it to a var and pass this var in the next line 
+      CodeSamples.const_set(file_name, klass)
+    puts klass.name
     puts klass.methods.map{ |m| m.to_s} 
   end
 end
