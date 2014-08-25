107a7d2ab5eb01ce931b5e7b86a697ef8432c495
Magdalena <madziaf86@tlen.pl>
2014-08-25 16:55:17 +0200

    TestConverter returns status and assertion

--- lib/ataru/test_converter.rb
+++ lib/ataru/test_converter.rb
@@ -5,17 +5,21 @@ class TestConverter
       raise ArgumentError
     end
     before_match = code_sample.match(/(.*) #=>/)
-    if before_match == nil 
-      return nil
+    after_match = code_sample.match(/#=> (.*)/)
+
+    if before_match == nil && after_match == nil
+      other_than_nil = [:nth_to_convert,code_sample ]
+      return other_than_nil
+    elsif before_match == nil && after_match != nil
+      return [:warning, code_sample]
     end
     before = before_match[1]
-    after_match = code_sample.match(/#=> (.*)/)
     after = after_match[1]
 
     # end
     # if before == nil || after_match == nil
     #   nil
     #
-    "assert_equal #{after}, #{before}"
+    [:ok, "assert_equal #{after}, #{before}"]
   end
 end
--- test/test_converter_test.rb
+++ test/test_converter_test.rb
@@ -6,25 +6,27 @@ class TestConverterTest < MiniTest::Test
   def test_no_hash
     code = "puts 'blah'"
     converted = TestConverter.convert(code)
-    assert_equal nil, converted
+    other_then_nil = [:nth_to_convert, code]
+    assert_equal other_then_nil, converted
   end
 
   def test_one_hash
     code = "a #=> 5"
     converted = TestConverter.convert(code)
-    assert_equal "assert_equal 5, a", converted
+    assert_equal [:ok, "assert_equal 5, a"], converted
   end
 
   def test_with_line_break
     code = "a = 1 + 2\na #=> 3"
     converted = TestConverter.convert(code)
-    assert_equal "assert_equal 3, a", converted
+    assert_equal [:ok, "assert_equal 3, a"], converted
   end
 
   def test_no_value_before_hash
     code = "#=> 3"
     converted = TestConverter.convert(code)
-    assert_equal nil, converted
+    other_then_nil = [:warning, code]
+    assert_equal other_then_nil, converted
   end
 
   def test_nil_as_parameter
