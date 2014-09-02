e2300e29beaf6d924148d85951df4870b56de638
Magdalena <madziaf86@tlen.pl>
2014-09-02 15:32:26 +0200

    extend LOADPATH with lib folder

--- lib/ataru.rb
+++ lib/ataru.rb
@@ -1,4 +1,11 @@
 require "ataru/version"
+require 'ataru/markdown_loader'
+require 'ataru/traverser'
+require 'ataru/code_samples'
+require 'ataru/argument_checker'
+require 'ataru/test_converter'
+require 'ataru/application'
+
 
 module Ataru
 # comes here sth?
