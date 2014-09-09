062e3bed57eb9dce4c1ced609ef664101c0fb387
Magdalena <madziaf86@tlen.pl>
2014-09-09 18:47:25 +0200

    added empty setup method

--- lib/ataru/cli/application.rb
+++ lib/ataru/cli/application.rb
@@ -4,6 +4,11 @@ require 'ataru'
 module Ataru
   module CLI
     class Application < Thor
+
+      desc "setup", "Create the setup file containing all dependencies for running code snippets"
+      def setup
+        File.new()
+      end
       desc "gogo FILENAMES", "Run and validate code samples in given markdown files"
       def gogo(*filenames)
         Ataru::ArgumentChecker.new(filenames).each do |checked_file|
