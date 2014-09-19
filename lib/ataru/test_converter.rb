5f65c2021636cd752db11354295b4759fc36e746
Magdalena <madziaf86@tlen.pl>
2014-09-19 22:31:07 +0200

    change replacement string in RegExp TestConverter

--- lib/ataru/test_converter.rb
+++ lib/ataru/test_converter.rb
@@ -2,7 +2,7 @@ module Ataru
   class TestConverter
 
     def self.convert(code)
-      code.gsub(/(^.+) # ?=> (.+$)/, 'assert_equal \2, \1')
+      code.gsub(/(^.+) # ?=> (.+$)/, 'assert_equal(\2, \1)')
     end
   end
 end
--- test/test_converter_test.rb
+++ test/test_converter_test.rb
@@ -13,36 +13,36 @@ class TestConverterTest < MiniTest::Test
   def test_hash_without_context_hash_without_space
     code_sample = CodeSample.new("a #=> 5", "test.md", 2)
     converted = TestConverter.convert(code_sample.code)
-    assert_equal "assert_equal 5, a", converted
+    assert_equal "assert_equal(5, a)", converted
   end
 
   def test_hash_without_context_hash_with_space
     code_sample = CodeSample.new("a # => 5", "test.md", 2)
     converted = TestConverter.convert(code_sample.code)
-    assert_equal "assert_equal 5, a", converted
+    assert_equal "assert_equal(5, a)", converted
   end
 
   def test_hash_with_context_hash_without_space
     code_sample = CodeSample.new("a = 1 + 2\na #=> 3", "test.md", 2)
     converted = TestConverter.convert(code_sample.code)
-    assert_equal "a = 1 + 2\nassert_equal 3, a", converted
+    assert_equal "a = 1 + 2\nassert_equal(3, a)", converted
   end
 
   def test_hash_with_context_hash_with_space
     code_sample = CodeSample.new("a = 1 + 2\na # => 3", "test.md", 2)
     converted = TestConverter.convert(code_sample.code)
-    assert_equal "a = 1 + 2\nassert_equal 3, a", converted
+    assert_equal "a = 1 + 2\nassert_equal(3, a)", converted
   end
 
   def test_assertion_in_the_middle
     code_sample = CodeSample.new("a = 1 + 2\na # => 3\nb = 2", "test.md", 2)
     converted = TestConverter.convert(code_sample.code)
-    assert_equal "a = 1 + 2\nassert_equal 3, a\nb = 2", converted
+    assert_equal "a = 1 + 2\nassert_equal(3, a)\nb = 2", converted
   end
 
   def test_assertion_at_the_end
     code_sample = CodeSample.new("a = 1 + 2\nb = 2\na # => 3", "test.md", 2)
     converted = TestConverter.convert(code_sample.code)
-    assert_equal "a = 1 + 2\nb = 2\nassert_equal 3, a", converted
+    assert_equal "a = 1 + 2\nb = 2\nassert_equal(3, a)", converted
   end
 end
