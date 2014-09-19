6c11e45da3f4bdc209d2cb893917236a246e2196
Magdalena <madziaf86@tlen.pl>
2014-09-19 15:26:19 +0200

    added tests to TestConverter checking for assertions in different lines of code

--- lib/ataru/test_converter.rb
+++ lib/ataru/test_converter.rb
@@ -2,9 +2,7 @@ module Ataru
   class TestConverter
 
     def self.convert(code)
-
       code.gsub(/(\S+) # ?=> (\S+)/, 'assert_equal \2, \1')
-
     end
   end
 end
--- test/test_converter_test.rb
+++ test/test_converter_test.rb
@@ -33,4 +33,16 @@ class TestConverterTest < MiniTest::Test
     converted = TestConverter.convert(code_sample.code)
     assert_equal "a = 1 + 2\nassert_equal 3, a", converted
   end
+
+  def test_assertion_in_the_middle
+    code_sample = CodeSample.new("a = 1 + 2\na # => 3\nb = 2", "test.md", 2)
+    converted = TestConverter.convert(code_sample.code)
+    assert_equal "a = 1 + 2\nassert_equal 3, a\nb = 2", converted
+  end
+
+  def test_assertion_at_the_end
+    code_sample = CodeSample.new("a = 1 + 2\nb = 2\na # => 3", "test.md", 2)
+    converted = TestConverter.convert(code_sample.code)
+    assert_equal "a = 1 + 2\nb = 2\nassert_equal 3, a", converted
+  end
 end
