8939ad95744b19d6158c1300c01364753f476ff0
Magdalena <madziaf86@tlen.pl>
2014-09-19 14:49:14 +0200

    adjusted TestConverter tests

--- test/test_converter_test.rb
+++ test/test_converter_test.rb
@@ -6,31 +6,31 @@ class TestConverterTest < MiniTest::Test
 
   def test_no_hash
     code_sample = CodeSample.new("puts 'blah'", "test.md", 2)
-    converted = TestConverter.convert(code_sample)
+    converted = TestConverter.convert(code_sample.code)
     assert_equal "puts 'blah'", converted
   end
 
   def test_hash_without_context_hash_without_space
     code_sample = CodeSample.new("a #=> 5", "test.md", 2)
-    converted = TestConverter.convert(code_sample)
+    converted = TestConverter.convert(code_sample.code)
     assert_equal "assert_equal 5, a", converted
   end
 
   def test_hash_without_context_hash_with_space
     code_sample = CodeSample.new("a # => 5", "test.md", 2)
-    converted = TestConverter.convert(code_sample)
+    converted = TestConverter.convert(code_sample.code)
     assert_equal "assert_equal 5, a", converted
   end
 
   def test_hash_with_context_hash_without_space
     code_sample = CodeSample.new("a = 1 + 2\na #=> 3", "test.md", 2)
-    converted = TestConverter.convert(code_sample)
+    converted = TestConverter.convert(code_sample.code)
     assert_equal "a = 1 + 2\nassert_equal 3, a", converted
   end
 
   def test_hash_with_context_hash_with_space
     code_sample = CodeSample.new("a = 1 + 2\na # => 3", "test.md", 2)
-    converted = TestConverter.convert(code_sample)
+    converted = TestConverter.convert(code_sample.code)
     assert_equal "a = 1 + 2\nassert_equal 3, a", converted
   end
 end
