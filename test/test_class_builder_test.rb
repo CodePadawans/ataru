a7eb8fa0a44a605cc2a4f3b935d041d2f7b477e9
Magdalena <madziaf86@tlen.pl>
2014-09-24 17:23:14 +0200

    deleted AtaruError as it produced unreadable error message

--- test/test_class_builder_test.rb
+++ test/test_class_builder_test.rb
@@ -1,7 +1,6 @@
 require 'minitest/autorun'
 require 'kramdown'
 require_relative '../lib/ataru/test_class_builder'
-require_relative '../lib/ataru/errors'
 require_relative '../lib/ataru/code_sample'
 
 class TestClassBuilderTest < MiniTest::Test
@@ -32,11 +31,9 @@ class TestClassBuilderTest < MiniTest::Test
 
   def test_invalid_code_sample
     code_samples = [CodeSample.new("a + 1", "my_test.md", 5)]
-    message = AtaruError.new("In the file: #{code_samples[0].file}, in line: #{code_samples[0].line_number}, is raising an error:")
     klass = TestClassBuilder.new(code_samples).build_test_class
 
-    assert_raises(AtaruError) { klass.new(@name_of_the_runnable).send(:test_my_test_0) }
-    assert_equal "In the file: my_test.md, in line: 5, is raising an error:", message.to_s
+    assert_raises(NameError) { klass.new(@name_of_the_runnable).send(:test_my_test_0) }
   end
 
 end
