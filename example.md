0d67588ae2a709c6a8c20b5fd564a472b0ce428c
Magdalena <madziaf86@tlen.pl>
2014-09-30 16:13:56 +0200

    `Setup` module included only if `ataru_setup.rb` exists

---
+++ example.md
@@ -0,0 +1,12 @@
+```ruby
+def bad
+  false
+end
+
+def good
+  true
+end
+
+bad #=> true
+good #=> true
+```
--- lib/ataru/test_class_builder.rb
+++ lib/ataru/test_class_builder.rb
@@ -13,7 +13,8 @@ module Ataru
     def build_test_class
       samples = code_samples
       klass = Class.new(MiniTest::Test) do
-        include Setup
+        path = Dir.pwd + '/ataru_setup.rb'
+        include Setup if File.exist?(path)
         samples.each_with_index do |sample, index|
           basename = Pathname.new(sample.file).basename(".*").to_s
           define_method("test_#{basename}_#{index}") do
